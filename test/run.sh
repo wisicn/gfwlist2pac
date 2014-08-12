8b66ec0672721374aa874731a9898670f178d598
Wissen Xue <wisicn@gmail.com>
2014-08-12 16:29:32 +0800

    change to socks proxy

--- test/run.sh
+++ test/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gfwlist2pac -f ./proxy.pac -p 'PROXY 127.0.0.1:8118; DIRECT;' --user-rule ./user_rule.txt
+gfwlist2pac -f ./proxy.pac -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;' --user-rule ./user_rule.txt
