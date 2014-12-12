7bba310b1864fc625a7bdbb10842ae42b5b28595
Wissen Xue <wisicn@gmail.com>
2014-12-13 00:21:05 +0800

    add the local LAN proxy

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gfwlist2pac -f ./proxy-socks.pac -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;' --user-rule ./user_rule.txt
 gfwlist2pac -f ./proxy.pac -p 'PROXY 127.0.0.1:8118; DIRECT;' --user-rule ./user_rule.txt
+gfwlist2pac -f ./wpad.dat -p 'PROXY 192.168.1.1:8118; DIRECT;' --user-rule ./user_rule.txt
