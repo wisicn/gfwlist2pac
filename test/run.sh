888e748736dcf65939313d8724f4a45d12ccea2c
Wissen Xue <wisicn@gmail.com>
2014-12-13 23:29:31 +0800

    typo of the proxy

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gfwlist2pac -f ./proxy-socks.pac -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;' --user-rule ./user_rule.txt
 gfwlist2pac -f ./proxy.pac -p 'PROXY 127.0.0.1:8118; DIRECT;' --user-rule ./user_rule.txt
-gfwlist2pac -f ./wpad.dat -p 'PROXY 192.168.1.1:8118; DIRECT;' --user-rule ./user_rule.txt
+gfwlist2pac -f ./wpad.dat -p 'PROXY 10.10.88.1:8118; DIRECT;' --user-rule ./user_rule.txt
 sed -i 's/amazon.com/bbc.co.uk/g' ./proxy.pac ./proxy-socks.pac ./wpad.dat
